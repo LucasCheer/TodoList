384114832049cfff017c34cad5ac86153b99b4c0
Lucas <cheroyonatan2@gmail.com>
2024-12-08 22:43:20 -0300

    Estructura logica de form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
+const [todoField, setTodoField] = useState('');
+
 const TodoForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="todoField" />
-        <button type="submit">Enviar</button>
+        <button type="submit">Agregar</button>
       </form>
     </>
   );
