d6dad387da42b7b300b83ec06a32496437278b0d
Lucas Viveros <lnatanviveros@gmail.com>
2024-12-18 20:51:42 -0300

    formateando el codigo y agregando propiedad a las tareas para la habilitacion de tareas

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,26 +1,29 @@
-import { useState } from 'react';
-import TodoItem from './TodoItem';
+import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 const TodoForm = () => {
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (todo.trim() === '') return;
-    setTodos([...todos, {id: Date.now(), text: todo, completed: false}]); 
+    if (todo.trim() === "") return;
+    setTodos([
+      ...todos,
+      { id: Date.now(), text: todo, completed: false, isEditing: false },
+    ]);
     console.log(todos);
-    setTodo('');
+    setTodo("");
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          name="todoField" 
-          value={todo} 
-          onChange={(e) => setTodo(e.target.value)} 
+        <input
+          type="text"
+          name="todoField"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
         />
         <button type="submit">Agregar</button>
       </form>
